84970042699137275237807b8fd76eba4b6a217b
MbaliPuso <mbalipuso@gmail.com>
2024-07-30 10:16:20 +0200

    home page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import AboutUs from '../components/AboutUs';
+import WhyUs from '../components/WhyUs';
+import Menu from '../components/Menu';
+import Specials from '../components/Specials';
+import Events from '../components/Events';
+import BookTable from '../components/BookTable';
+import Testimonials from '../components/Testimonials';
+import Gallery from '../components/Gallery';
+import Chefs from '../components/Chefs';
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer';
+
+const Homepage = () => {
+    return (
+        <>
+        <Header />
+        <Hero />
+        <AboutUs />
+        <WhyUs />
+        <Menu />
+        <Specials />
+        <Events />
+        <BookTable />
+        <Testimonials />
+        <Gallery />
+        <Chefs />
+        <ContactUs />
+        <Footer />
+        </>
+    );
+};
+
+export default Homepage;
